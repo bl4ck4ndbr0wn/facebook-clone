3a4cd33d1951f4fd3a61d3dee1c8e42915ccbaa1
bl4ck4ndbr0wn <alphandirangu@gmail.com>
2020-12-18 02:18:14 +0300

    Added link to github repo

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -2,6 +2,7 @@ import {Avatar} from '@material-ui/core';
 import {ExpandMoreOutlined} from '@material-ui/icons';
 import ChatIcon from '@material-ui/icons/Chat';
 import EmojiFlagsIcon from '@material-ui/icons/EmojiFlags';
+import GitHubIcon from '@material-ui/icons/GitHub';
 import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
 import PeopleIcon from '@material-ui/icons/People';
 import StorefrontIcon from '@material-ui/icons/Storefront';
@@ -9,13 +10,15 @@ import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
 import React from 'react';
 import {useStateValue} from '../../StateProvider';
 
-const SidebarRow = ({src, Icon, title, className = ''}) => {
+const SidebarRow = ({src, Icon, title, link, className = '',linkClassName}) => {
   return (
-    <div className="sidebarRow">
+    // <div className={`sidebarRow ${linkClassName}`}>
+      <div className="sidebarRow" id={linkClassName}>
       {src && <Avatar src={src}/>}
       {Icon && <Icon className={className}/>}
 
-      <h4>{title}</h4>
+      {title && <h4>{title}</h4>}
+      {link && <h4><a href={link}>Link to Repository</a></h4>}
     </div>
   );
 }
@@ -25,6 +28,11 @@ const Sidebar = () => {
 
   return (
     <div className="sidebar">
+      <SidebarRow
+        Icon={GitHubIcon}
+        link="https://github.com/bl4ck4ndbr0wn/facebook-clone"
+        linkClassName="sidebar__row--important"
+      />
       <SidebarRow
         src={state.user.photoURL}
         title={state.user.displayName}
